cc4232b66dfd8ea77e4d399236c85a74970b8f12
jerrodmpackard <jerrodmpackard@gmail.com>
2024-01-08 13:59:22 -0800

    implemented peer review suggestions. eliminated unnecessary variable

--- Controllers/SayHello.cs
+++ Controllers/SayHello.cs
@@ -4,15 +4,15 @@ namespace PackardJSayHello_Endpoint.Controllers;
 
 [ApiController]
 [Route("[Controller]")]
+
 public class SayHello : ControllerBase
 {
-    public string userName = "";
-
     [HttpPost]
     [Route("AddName/{newName}")]
+
     public string AddName(string newName)
     {
-        userName = newName;
+        string userName = newName;
         return $"Hello, {userName}. Nice to meet you!";
     }
 }
