88b3539421495f9e79365fdd40f5c16084da0721
jerrodmpackard <jerrodmpackard@gmail.com>
2024-01-08 12:22:27 -0800

    fixed postman issue

--- Controllers/SayHello.cs
+++ Controllers/SayHello.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace PackardJSayHello_Endpoint.Controllers;
 
 [ApiController]
-[Route("Controller")]
+[Route("[Controller]")]
 public class SayHello : ControllerBase
 {
     public string userName = "";
 
     [HttpPost]
-    [Route("AddName")]
+    [Route("AddName/{newName}")]
     public string AddName(string newName)
     {
         userName = newName;
