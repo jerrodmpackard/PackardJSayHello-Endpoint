b831753331db40f2d895c8a93c6f4f4389035e48
jerrodmpackard <jerrodmpackard@gmail.com>
2024-01-08 12:01:39 -0800

    initial commit

---
+++ Controllers/SayHello.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PackardJSayHello_Endpoint.Controllers;
+
+[ApiController]
+[Route("Controller")]
+public class SayHello : ControllerBase
+{
+    public string userName = "";
+
+    [HttpPost]
+    [Route("AddName")]
+    public string AddName(string newName)
+    {
+        userName = newName;
+        return $"Hello, {userName}. Nice to meet you!";
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Jerrod Packard
+January 8, 2024
+Say Hello - Endpoint
+Created an endpoint to take in a user's name and return a greeting.
+Peer review by: 
+Peer review comments: 
